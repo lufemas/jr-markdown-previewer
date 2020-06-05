46d7c9145e6bbedd3165e924dc98ef5b32020db4
lufemas <maschietto.jr@gmail.com>
2020-06-04 22:26:55 -0400

    build

---
+++ build/precache-manifest.c87906fa9b7871bf12b363a329683ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0d490691c52e27918f3a3eb38624e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca7d7b41235847fc8a1",
+    "url": "/static/css/main.e17182d4.chunk.css"
+  },
+  {
+    "revision": "aab09309964fc96e9737",
+    "url": "/static/js/2.2c1f2d73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c1f2d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca7d7b41235847fc8a1",
+    "url": "/static/js/main.467a0105.chunk.js"
+  },
+  {
+    "revision": "ea385387e49b13009757",
+    "url": "/static/js/runtime-main.9ed50ec1.js"
+  }
+]);
